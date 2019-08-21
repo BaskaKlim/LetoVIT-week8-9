c9841be2f5bab8870b7ee976c21190906c59cf19
unknown <baska.klimekova@gmail.com>
2019-08-21 12:38:00 +0200

    fix navigation

--- FE/todo-app/src/components/Navbar.js
+++ FE/todo-app/src/components/Navbar.js
@@ -4,7 +4,27 @@ import { Link } from 'react-router-dom';
 class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar light bg-light">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                <Link class="nav-link" to="/">
+                                TODOs <span class="sr-only">(current)</span> </Link>
+                </li>
+                <li class="nav-item">
+                <Link class="nav-link" to="/add">
+                                ADD TODO
+                                </Link>
+            
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+            /*<nav className="navbar navbar-expand-lg navbar light bg-light">
                 <div className="collapse navbar-collapse">
                     <ul className="navbar-nav">
 
@@ -24,6 +44,7 @@ class Navbar extends Component {
 
 
             </nav>
+            */
         )
     }
 }
--- FE/todo-app/src/index.js
+++ FE/todo-app/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper.min';
+import 'bootstrap/dist/js/bootstrap.min';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
